2ebac71db792d08b6ca44b163ebe12cdf3af9b6d
Santosh Nagargoje <santosh.nag2217@gmail.com>
2025-01-02 21:01:39 +0530

    Fixed code checker issues

--- tests/output/main_test.php
+++ tests/output/main_test.php
@@ -13,6 +13,7 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
 namespace block_site_stats\output;
 
 use advanced_testcase;
