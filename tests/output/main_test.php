e75c0794195b8b08b62541ccd5fc8fc9d2526424
Santosh Nagargoje <santosh.nag2217@gmail.com>
2025-04-21 16:07:23 +0530

    Fixed unit test issues

--- tests/output/main_test.php
+++ tests/output/main_test.php
@@ -38,6 +38,7 @@ final class main_test extends advanced_testcase {
      * @return void
      */
     public function setUp(): void {
+        parent::setUp();
         $this->resetAfterTest();
         $this->setAdminUser();
         $this->main = new main();
--- tests/site_stats_test.php
+++ tests/site_stats_test.php
@@ -36,6 +36,7 @@ final class site_stats_test extends advanced_testcase {
     public static function setUpBeforeClass(): void {
         require_once(__DIR__ . '/../../moodleblock.class.php');
         require_once(__DIR__ . '/../block_site_stats.php');
+        parent::setUpBeforeClass();
     }
 
     /**
