1bb1b73b670256bb5d4cbc158531d62b5115e06e
Santosh Nagargoje <santosh.nag2217@gmail.com>
2025-10-07 18:12:49 +0530

    Added moodle 51 compatibility changes

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2025042100;        // The current plugin version (Date: YYYYMMDDXX).
-$plugin->release = '1.0.3';
+$plugin->version   = 2025100700;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->release = '1.0.4';
 $plugin->requires  = 2022111800;        // Requires this Moodle version.
 $plugin->component = 'block_site_stats'; // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_STABLE;
